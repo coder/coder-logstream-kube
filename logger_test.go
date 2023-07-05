162d0264911038612cc074ee60fb6dd3a15a17a0
Kyle Carberry <kyle@carberry.com>
2023-07-05 22:47:55 +0000

    feat: add support for logging events from ReplicaSets
    Fixes #3

--- logger_test.go
+++ logger_test.go
@@ -13,12 +13,103 @@ import (
 	"github.com/coder/coder/codersdk/agentsdk"
 	"github.com/stretchr/testify/require"
 	"github.com/zeebo/assert"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/fake"
 )
 
-func TestPodEventLogger(t *testing.T) {
+func TestReplicaSetEvents(t *testing.T) {
+	t.Parallel()
+
+	queued := make(chan agentsdk.PatchStartupLogs, 1)
+	agent := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var req agentsdk.PatchStartupLogs
+		err := json.NewDecoder(r.Body).Decode(&req)
+		assert.NoError(t, err)
+		queued <- req
+	}))
+	agentURL, err := url.Parse(agent.URL)
+	require.NoError(t, err)
+	namespace := "test-namespace"
+	client := fake.NewSimpleClientset()
+	ctx := context.Background()
+	reporter, err := newPodEventLogger(ctx, podEventLoggerOptions{
+		client:      client,
+		coderURL:    agentURL,
+		namespace:   namespace,
+		logger:      slogtest.Make(t, nil),
+		logDebounce: time.Millisecond,
+	})
+	require.NoError(t, err)
+
+	rs := &appsv1.ReplicaSet{
+		ObjectMeta: v1.ObjectMeta{
+			Name: "test-rs",
+			CreationTimestamp: v1.Time{
+				Time: time.Now().Add(time.Hour),
+			},
+		},
+		Spec: appsv1.ReplicaSetSpec{
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: v1.ObjectMeta{
+					Name: "test-pod",
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{{
+						Env: []corev1.EnvVar{
+							{
+								Name:  "CODER_AGENT_TOKEN",
+								Value: "test-token",
+							},
+						},
+					}},
+				},
+			},
+		},
+	}
+	_, err = client.AppsV1().ReplicaSets(namespace).Create(ctx, rs, v1.CreateOptions{})
+	require.NoError(t, err)
+
+	log := <-queued
+	require.Len(t, log.Logs, 1)
+	require.Contains(t, log.Logs[0].Output, "Queued pod from ReplicaSet")
+
+	event := &corev1.Event{
+		ObjectMeta: v1.ObjectMeta{
+			Name: "test-event",
+			CreationTimestamp: v1.Time{
+				Time: time.Now().Add(time.Hour),
+			},
+		},
+		InvolvedObject: corev1.ObjectReference{
+			Kind: "Pod",
+			Name: "test-pod",
+		},
+		Reason:  "Test",
+		Message: "Test event",
+	}
+	_, err = client.CoreV1().Events(namespace).Create(ctx, event, v1.CreateOptions{})
+	require.NoError(t, err)
+
+	log = <-queued
+	require.Len(t, log.Logs, 1)
+	require.Contains(t, log.Logs[0].Output, event.Message)
+
+	err = client.AppsV1().ReplicaSets(namespace).Delete(ctx, rs.Name, v1.DeleteOptions{})
+	require.NoError(t, err)
+
+	require.Eventually(t, func() bool {
+		reporter.mutex.Lock()
+		defer reporter.mutex.Unlock()
+		return len(reporter.podToAgentTokens) == 0 && len(reporter.replicaSetToTokens) == 0
+	}, time.Second, time.Millisecond)
+
+	err = reporter.Close()
+	require.NoError(t, err)
+}
+
+func TestPodEvents(t *testing.T) {
 	t.Parallel()
 
 	queued := make(chan agentsdk.PatchStartupLogs, 1)
